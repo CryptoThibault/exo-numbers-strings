493a24f4745a4ba2d19ce7d287ba179409ddb00f
CryptoThibault <thibault.chalaoux@gmail.com>
2021-02-16 15:34:09 +0100

    1,2,3

---
+++ convert.js
@@ -0,0 +1,8 @@
+let nb = [10, 15, 16, 5005, 52390903]
+for (let elem of nb) {
+  console.log(elem.toString(2))
+} for (let elem of nb) {
+  console.log(elem.toString(8))
+} for (let elem of nb) {
+  console.log(elem.toString(16))
+}
---
+++ decimal.js
@@ -0,0 +1,4 @@
+let nb = [0x123, 0123, 0b10011001, 0xdeadbeef, 0xea7beef, 0b1111111111111111, 0777]
+for (let elem of nb) {
+  console.log(elem.toString())
+}
